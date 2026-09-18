28659711e75ef905a2045c8d9fb315c02c55303b
VARSHA S K <66677340+skvarshasivakumar@users.noreply.github.com>
2026-09-18 15:44:48 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    // Reanimated v4 moved the Babel plugin into react-native-worklets.
+    // Using the old 'react-native-reanimated/plugin' name silently breaks
+    // worklet compilation on Android, which crashes expo-router / react-native-screens
+    // at mount time ("opens glitched, then closes").
+    // This plugin MUST be listed last.
+    plugins: ['react-native-worklets/plugin'],
   };
 };
